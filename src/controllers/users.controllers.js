161606de1cf75285a4025d5098a8b066a080c743
chakravarthi <chakrijallu7@gmail.com>
2024-02-05 20:19:18 +0530

    debugged the user register

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,42 +1,56 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
-import {ApiResponse} from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/users.models.js";
-import {ApiError} from "../utils/ApiError.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (request, response) => {
+  const { username, password, email } = request.body;
 
-    const { username, password, email } = request.body;
+  // check if user is already exists
 
-    // check if user is exists
+  const userExists = await User.findOne({
+    $or: [{ email }, { username }],
+  });
 
-    const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new ApiError(409, "User with email or username already exists.");
+  }
 
-    if (userExists) {
-        throw new ApiError(400, "User Already Exists.");
-    }
+  // create the user model
+  const user = await User.create({
+    username,
+    email,
+    password,
+  });
 
-    // create the user model
-    const user = await User.create({username, email, password});
+  if (!user) {
+    throw new ApiError(400, "User Creation Failed.");
+  }
 
-    if (!user) {
-        throw new ApiError(400, "User Creation Failed.");
-    }
+  const createdUser = await User.findById(user._id).select("-password");
 
-    // token
-    const access_token = user.accessToken();
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user.");
+  }
 
-    if (!access_token) {
-        throw new ApiError(400, "Token Creation Failed.");
-    }
+  return response
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered Successfully"));
+});
 
-    const createdUser = await User.findById(user._id).select("-password");
+const loginUser = asyncHandler(async (request, response) => {
 
-    if (!createdUser) {
-        throw new ApiError(400, "User Creation Failed.");   
-    }
+    const {email, password} = request.body;
 
-    return response.status(201).json(new ApiResponse(201, {  access_token, createdUser} ,"Register Successful"))
-})
+  // token
+  const access_token = user.accessToken();
 
+  if (!access_token) {
+    throw new ApiError(400, "Token Creation Failed.");
+  }
+  return response
+    .status(200)
+    .json(new ApiResponse(200, {}, "User loggedIn Successfully"));
+});
 
-export { registerUser }
+export { registerUser, loginUser };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/users.controllers.js";
-import { signUpValidation } from "../validators/users.validators.js";
+import { loginUser, registerUser } from "../controllers/users.controllers.js";
+import { signInValidation, signUpValidation } from "../validators/users.validators.js";
 import { validate } from "../middlewares/validate.middlewares.js";
 
 const router = express.Router();
 
 router.route("/register").post(validate(signUpValidation), registerUser);
+
+router.route("/login").post(validate(signInValidation), loginUser);
 
 export default router;
