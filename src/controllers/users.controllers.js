0d2d46dd67e1f652ca890db01db6948b821d8ab6
chakravarthi <chakrijallu7@gmail.com>
2024-02-06 18:54:17 +0530

    debug logout and refresh Token

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -12,6 +12,9 @@ const generateAccessAndRefreshToken = async (userId) => {
         const accessToken = user.generateAccessToken();
         const refreshToken = user.generateRefreshToken();
 
+        // user.refreshToken = refreshToken;
+        // await user.save({validateBeforeSave: false });
+
         await User.findByIdAndUpdate(user._id, {
           $set: {
             refreshToken: refreshToken
@@ -53,7 +56,7 @@ const registerUser = asyncHandler(async (request, response) => {
     throw new ApiError(400, "User Creation Failed.");
   }
 
-  const createdUser = await User.findById(user._id).select("-password");
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user.");
@@ -97,7 +100,7 @@ const loginUser = asyncHandler(async (request, response) => {
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
 
-    const loggedInUser = await User.findById(user._id).select("-password")
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
     const options = {
         httpOnly: true,
@@ -151,45 +154,53 @@ const getUserInfo = asyncHandler(async (request, response) => {
   Refresh Token
 */
 
-const refreshToken = asyncHandler(async (request, response) => {
-  const userExisted = await User.findOne({ refreshToken: request.body.refresh_token })
+const refreshAccessToken = asyncHandler(async (request, response) => {
+  try {
+    const incomingRefreshToken = request.cookies.refreshToken || request.body.refresh_token
 
-  if (!userExisted) {
-    throw new ApiError(401, "Invalid refresh token.")
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request")
   }
 
-  const { id } = jwt.verify(userExisted.refreshToken, SECRET_REFRESH_TOKEN)
+  const decodedToken = jwt.verify(
+    incomingRefreshToken,
+    SECRET_REFRESH_TOKEN
+  )
 
-  const user = await User.findById(id);
+  const user = await User.findById(decodedToken?.id)
 
   if (!user) {
-    throw new ApiError(404, "User Not Found!")
+    throw new ApiError(401, "Invalid refresh token")
   }
 
-  const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
+  if (incomingRefreshToken !== user?.refreshToken) {
+    throw new ApiError(401, "Refresh token is expired or used")
+  }
 
-  // const loggedInUser = await User.findById(user._id).select("-password")
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
 
-    const options = {
-        httpOnly: true,
-        secure: true
-    }
+  const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshToken(user._id)
 
-    return response
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
-        new ApiResponse(
-            200, 
-            {
-                // user: loggedInUser,
-                accessToken,
-                refreshToken
-            }
-        )
+  return response
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", newRefreshToken, options)
+  .json(
+    new ApiResponse(
+      200,
+      { accessToken, refreshToken: newRefreshToken },
+      "Access token refreshed"
     )
-
+  )
+  } catch (error) {
+    throw new ApiError(
+      401, 
+      error?.message || "Invalid refresh token"
+      )
+  }
 
 }) 
 
@@ -233,5 +244,5 @@ export {
     loginUser, 
     logoutUser, 
     getUserInfo,
-    refreshToken
+    refreshAccessToken
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserInfo, loginUser, logoutUser, refreshToken, registerUser } from "../controllers/users.controllers.js";
+import { getUserInfo, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/users.controllers.js";
 import { signInValidation, signUpValidation, refreshTokenValidation } from "../validators/users.validators.js";
 import { validate } from "../middlewares/validate.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -10,12 +10,13 @@ router.route("/register").post(validate(signUpValidation), registerUser);
 
 router.route("/login").post(validate(signInValidation), loginUser);
 
-router.route("/refresh").post(validate(refreshTokenValidation), refreshToken);
+router.route("/refresh").post(refreshAccessToken);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/me").get(verifyJWT, getUserInfo);
+
 
 
 export default router;
